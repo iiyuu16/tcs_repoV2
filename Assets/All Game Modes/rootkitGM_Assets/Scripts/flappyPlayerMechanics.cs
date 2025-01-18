926f34ab68a8dfecc17391420505da397a4dc645
iiyuu16 <pcg4m3r16@gmail.com>
2025-01-18 22:55:40 +0800

    added trueEnding scene, visnov_bots
    to do:
    - change gm bgms pitch, volume, sfx
    - make new script for disabling gameobjs after trueEnding & goodEnding
    - make goodEnding scene

---
+++ Assets/All Game Modes/rootkitGM_Assets/Scripts/flappyPlayerMechanics.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class flappyPlayerMechanics : MonoBehaviour
+{
+    private float speed;
+    public float target = 0.01f;
+    public float jumpStrength = -0.01f;
+    public float weight = 0.01f;
+    public KeyCode jumpKey;
+    public flappySoundSource soundSource;
+
+    private float currentZRotation;
+    private float targetZRotation = -100f;
+
+    void Update()
+    {
+        transform.position = new Vector3(0.06200001f, transform.position.y, -2.6f);
+
+        transform.Translate(speed, 0, 0);
+        speed = Mathf.Lerp(speed, target, weight);
+
+        if (Input.GetKeyDown(jumpKey))
+        {
+            speed = jumpStrength;
+            soundSource.jumpsSFX();
+            Debug.Log("Jump");
+            currentZRotation = -80f;
+        }
+
+        currentZRotation = Mathf.Lerp(currentZRotation, targetZRotation, weight);
+        transform.localEulerAngles = new Vector3(0, -90, currentZRotation);
+    }
+}
