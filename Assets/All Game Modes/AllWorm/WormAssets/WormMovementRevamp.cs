6aaba948c3de215cf7a413e6430fd5309afe070a
ShadowIWS <rocheandre123@gmail.com>
2025-01-29 19:13:32 +0800

    Update WormMovementRevamp.cs

--- Assets/All Game Modes/AllWorm/WormAssets/WormMovementRevamp.cs
+++ Assets/All Game Modes/AllWorm/WormAssets/WormMovementRevamp.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 using UnityEngine.Animations;
 using UnityEngine.InputSystem;
 using UnityEngine.ProBuilder.Shapes;
+using UnityEngine.XR.Interaction.Toolkit;
 
 
 public class WormMovementRevamp : MonoBehaviour
