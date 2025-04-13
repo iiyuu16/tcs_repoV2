ec5a99917286578ce120a862564e56df554cb65e
iiyuu16 <pcg4m3r16@gmail.com>
2025-04-13 22:08:09 +0800

    TCS revisions (cont)
    botsGM:
    -added enemydeath vfx
    
    visnovMain:
    -notification dot shows after a mission

--- Assets/All Game Modes/botsGM_Assets/scripts/registerHit.cs
+++ Assets/All Game Modes/botsGM_Assets/scripts/registerHit.cs
@@ -4,11 +4,34 @@ using UnityEngine;
 
 public class registerHit : MonoBehaviour
 {
+    public GameObject objMesh;
+    public survivalSoundSource sfx;
+
+    public ParticleSystem sparksFX;
+    public ParticleSystem flashFX;
+    public ParticleSystem fireFX;
+    public ParticleSystem smokeFX;
+
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player Bullet")
         {
-            this.gameObject.SetActive(false);
+            objMesh.SetActive(false);
+            sparksFX.Play();
+            smokeFX.Play();
+            flashFX.Play();
+            fireFX.Play();
+            sfx.explosionSFX();
+            DelayDisable();
         }
     }
+
+    private IEnumerator DelayDisable()
+    {
+        yield return new WaitForSeconds(0.5f);
+        this.gameObject.SetActive(false);
+    }
+
+
+
 }
